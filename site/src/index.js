408876e333b27112e6c9ce76c0d9840a146df50e
Genesis Blockhead <genesisblockhead@protonmail.com>
2023-05-01 13:36:46 -0400

    basic folds

--- site/src/index.js
+++ site/src/index.js
@@ -1,9 +1,116 @@
+const async = require("async");
 const paper = require("paper");
 
 paper.install(window);
 
 const along = (a, b, factor) => a.add(b.subtract(a).multiply(factor));
 const midpoint = (a, b, factor) => along(a, b, 0.5);
+
+const divide = (p1, p2) => {
+  if (p1.x > p2.x) {
+    return divide(p2, p1);
+  }
+  const { width, height } = paper.view.bounds;
+  if (p1.x == p2.x) {
+    return new paper.Path.Rectangle([0, 0], [p1.x, height]);
+  } else if (p2.y == p1.y) {
+    return new paper.Path.Rectangle([0, 0], [width, p1.y]);
+  } else {
+    const m = (p2.y - p1.y) / (p2.x - p1.x);
+    const b = p2.y - m * p2.x;
+    let f = (x) => m * x + b;
+    let y = f(width);
+    let xIntercept = -b / m;
+    let heightIntercept = (height - b) / m;
+    if (b < 0) {
+      if (y < 0) {
+        throw new Error("impossible");
+      } else if (y < height) {
+        // triangle including (w, 0)
+        return new paper.Path({
+          segments: [
+            [xIntercept, 0],
+            [width, 0],
+            [width, y],
+          ],
+        });
+      } else {
+        return new paper.Path({
+          segments: [
+            [0, 0],
+            [xIntercept, 0],
+            [heightIntercept, height],
+            [0, height],
+          ],
+        });
+      }
+    } else if (b < height) {
+      if (y < 0) {
+        // triangle including origin
+        return new paper.Path({
+          segments: [
+            [0, 0],
+            [0, b],
+            [xIntercept, 0],
+          ],
+        });
+      } else if (y < height) {
+        return new paper.Path({
+          segments: [
+            [0, b],
+            [0, 0],
+            [width, 0],
+            [width, y],
+          ],
+        });
+      } else {
+        // triangle including (0, h)
+        return new paper.Path({
+          segments: [
+            [0, height],
+            [0, b],
+            [heightIntercept, height],
+          ],
+        });
+      }
+    } else {
+      // b > height
+      if (y < 0) {
+        return new paper.Path({
+          segments: [
+            [0, 0],
+            [xIntercept, 0],
+            [heightIntercept, height],
+            [0, height],
+          ],
+        });
+      } else if (y < height) {
+        // triangle including (h, w)
+        return new paper.Path({
+          segments: [
+            [heightIntercept, height],
+            [width, y],
+            [width, height],
+          ],
+        });
+      } else {
+        throw new Error("impossible");
+      }
+    }
+  }
+};
+
+const crease = (paths, a, b) => {};
+
+const clamp = (x) => {
+  if (x < 0) {
+    return 0;
+  }
+  if (x > 1) {
+    return 1;
+  }
+  return x;
+};
 
 const makeVectors = (path, destination) =>
   path.segments.map(({ point }, i) => destination[i].subtract(point));
@@ -12,33 +119,53 @@ const white = 1.0;
 const black = 0.0;
 
 // const edgeColor = 0.9;
+const lightGrey = 0.9;
 const edgeColor = black;
 const lightSide = white;
 const darkSide = 0.95;
 const creaseColor = edgeColor;
 
 const reflection = (path, a, b) => {
-  for (const p of [a, b]) {
-    new paper.Path.Circle({
-      center: p,
-      radius: 5,
-      fillColor: "lightblue",
-    });
-  }
   const p2 = path.clone();
-  const m = (b.y - a.y) / (b.x - a.x);
-  const reflectionMatrix = new paper.Matrix(
-    1 - Math.pow(m, 2),
-    2 * m,
-    2 * m,
-    Math.pow(m, 2) - 1,
-    0,
-    0
-  ).scale(1 / (1 + Math.pow(m, 2)));
-  console.log(a.x, a.y);
-  p2.translate(a.multiply(-1));
-  p2.transform(reflectionMatrix);
-  p2.translate(a);
+  if (b.x == a.x) {
+    p2.segments.forEach((s) => {
+      s.point.x = 2 * a.x - s.x;
+    });
+  } else {
+    const m = (b.y - a.y) / (b.x - a.x);
+    const reflectionMatrix = new paper.Matrix(
+      1 - Math.pow(m, 2),
+      2 * m,
+      2 * m,
+      Math.pow(m, 2) - 1,
+      0,
+      0
+    ).scale(1 / (1 + Math.pow(m, 2)));
+    p2.translate(a.multiply(-1));
+    p2.transform(reflectionMatrix);
+    p2.translate(a);
+  }
+  return p2;
+};
+
+const projection = (path, a, b) => {
+  const p2 = path.clone();
+  if (b.x == a.x) {
+    p2.segments.forEach((s) => {
+      s.point.x = a.x;
+    });
+  } else {
+    const slope = (b.y - a.y) / (b.x - a.x);
+    const theta = -Math.atan(slope);
+    const matrix = new paper.Matrix();
+    matrix.rotate(theta);
+    p2.transform(matrix);
+    p2.segments.forEach((s) => {
+      s.point.y = 0;
+    });
+    matrix.rotate(-2 * theta);
+    p2.transform(matrix);
+  }
   return p2;
 };
 
@@ -73,7 +200,7 @@ const phase1 = (startTime, [a, b, c, d]) => {
   tween.path = path;
 
   paper.view.onFrame = ({ time }) => {
-    let factor = Math.min(1.0, time - startTime);
+    let factor = clamp(time - startTime);
     tween.path.interpolate(tween.start, tween.end, factor);
     tween.path.visible = true;
     if (factor > 0.5) {
@@ -131,7 +258,7 @@ const phase2 = (startTime, [a, b, c]) => {
   }
 
   paper.view.onFrame = ({ delta, time, count }) => {
-    const factor = Math.min(1.0, (time - startTime) / 1.0);
+    const factor = clamp(time - startTime);
     creases.forEach(
       (crease) =>
         (crease.strokeColor = (1.0 - factor) * white + factor * creaseColor)
@@ -147,32 +274,78 @@ const phase2 = (startTime, [a, b, c]) => {
   };
 };
 
-const phase3 = (time, [a, b, c, d]) => {
-  paper.project.activeLayer.removeChildren();
+const phase3 = (startTime, [a, b, c, d]) => {
   paper.view.onFrame = () => {};
-  const rectangle = new paper.Path([a, b, c, d]);
-  rectangle.fillColor = darkSide;
 
-  const rightHalf = new paper.Path({
-    segments: [b, c, d],
-    fillColor: "green",
-  });
-  const leftHalf = new paper.Path({
-    segments: [b, a, d],
-    fillColor: "red",
+  paper.project.activeLayer.removeChildren();
+  const rectangle = new paper.Path({
+    segments: [a, b, c, d],
+    fillColor: darkSide,
+    strokeColor: edgeColor,
   });
 
   const p = along(b, d, 0.75);
-  const t2 = new paper.Path({
-    segments: [p, c, d],
-    fillColor: "orange",
+  const rightHalf = new paper.Path({
+    segments: [b, c, d],
+    fillColor: darkSide,
+    strokeColor: edgeColor,
+    closed: true,
   });
-  const t1 = new paper.Path({
-    segments: [b, c, p],
-    fillColor: "pink",
+  const leftHalf = new paper.Path({
+    segments: [p, a, d],
+    fillColor: darkSide,
+    strokeColor: edgeColor,
+    closed: true,
   });
 
-  const t3 = reflection(t1, c, p);
+  /*  const t1 = new paper.Path({
+    segments: [b, c, p],
+    fillColor: darkSide,
+    strokeColor: edgeColor,
+    closed: true,
+  });
+*/
+
+  const slice = divide(p, c);
+
+  const t1 = rightHalf.intersect(slice);
+  const y = rightHalf.subtract(slice);
+  t1.fillColor = "green";
+  y.fillColor = "yellow";
+
+  const t2 = new paper.Path({
+    segments: [b, a, p],
+    fillColor: darkSide,
+    strokeColor: edgeColor,
+    closed: true,
+  });
+
+  const tweens = [
+    {
+      path: t1,
+      start: t1.clone(),
+      end: reflection(t1, c, p),
+    },
+    {
+      path: t2,
+      start: t2.clone(),
+      end: reflection(t2, a, p),
+    },
+  ];
+  tweens.forEach(({ start, end }) => {
+    start.visible = false;
+    end.visible = false;
+  });
+
+  //  slice.opacity = 0.5;
+  //  slice.fillColor = "red";
+
+  t1.onFrame = ({ time }) => {
+    const factor = clamp(time);
+    tweens.forEach(({ path, start, end }) => {
+      path.interpolate(start, end, factor);
+    });
+  };
 
   return null;
 };
@@ -257,10 +430,118 @@ const f = () => {
   };
 };
 
+let drawPaper = (top, size) => {
+  let from = new paper.Point(top);
+  let to = from.add([size, size]);
+  let square = new paper.Path.Rectangle({
+    from,
+    to,
+    strokeColor: lightGrey,
+    fillColor: white,
+    opacity: 0.96,
+  });
+  return square;
+};
+
+const fold = (paths, pointToFold, creaseA, creaseB, cb) => {
+  async.waterfall(
+    [
+      (cb) => {
+        let cut = divide(creaseA, creaseB);
+        cut.fillColor = "red";
+        cut.opacity = 0.1;
+        cut.selected = true;
+        setTimeout(cb, 0, null, cut);
+      },
+      (cut, cb) => {
+        const lefts = [];
+        const rights = [];
+
+        let leftIsStationary = true;
+        for (const path of paths) {
+          const part1 = path.intersect(cut);
+          const part2 = path.subtract(cut);
+          lefts.push(part1);
+          rights.push(part2);
+          path.remove();
+          if (leftIsStationary && part1.contains(pointToFold)) {
+            leftIsStationary = false;
+          }
+        }
+        const [folds, stationary] = leftIsStationary
+          ? [rights, lefts]
+          : [lefts, rights];
+        let top = stationary[0];
+        const result = [];
+        for (const x of stationary) {
+          result.push(x);
+          console.log(x.fillColor);
+          x.bringToFront();
+        }
+        folds.reverse();
+        for (const partToFold of folds) {
+          const r = reflection(partToFold, creaseA, creaseB);
+          r.bringToFront();
+          result.push(r);
+          partToFold.remove();
+        }
+        //        cut.remove();
+        setTimeout(cb, 0, null, result);
+      },
+    ],
+    cb
+  );
+};
+
 window.onload = function () {
   paper.setup("myCanvas");
-  let square = getSquare(new Point(300, 300), 320);
-  paper.view.onFrame = ({ time, delta, count }) => {
-    phase1(time, square);
+  //  let square = getSquare(new Point(300, 300), 320);
+  //  paper.view.onFrame = ({ time, delta, count }) => {
+  //    phase1(time, square);
+  //  };
+
+  let size = 300;
+  let start = new paper.Point(100, 100);
+  let backToFront = [];
+  let width = 15;
+  for (let i = width; i < size; i = i + width) {
+    let line = new paper.Path.Rectangle(
+      new paper.Point(start.x + i, start.y),
+      new paper.Point(start.x + i + 1, start.y + size)
+    );
+    line.fillColor = "lightblue";
+    backToFront.push(line);
+    let line2 = new paper.Path.Rectangle(
+      new paper.Point(start.x, start.y + i),
+      new paper.Point(start.x + size, start.y + i + 1)
+    );
+    line2.fillColor = "lightblue";
+    backToFront.push(line2);
+  }
+
+  //  let back = drawPaper([325, 325], 250);
+  //  back.fillColor = "green";
+  let horizontalLines;
+  let front = drawPaper(start, 300);
+  //  square.fillColor = "red";
+  console.log(front);
+  backToFront.push(front);
+
+  let p1 = along(front.segments[0].point, front.segments[1].point, 1 / 3);
+  let p2 = along(front.segments[1].point, front.segments[2].point, 1 / 2);
+
+  const step1 = async.apply(fold, backToFront, start, p1, p2);
+  const step2 = (pieces, cb) => {
+    let p3 = along(p1, p2, 0.6);
+    let p4 = along(front.segments[2].point, front.segments[3].point, 0.5);
+
+    fold(pieces, front.segments[2].point, p3, p4, cb);
+    //cb(null);
   };
+
+  async.waterfall([step1, step2], (err) => {
+    if (err) {
+      throw err;
+    }
+  });
 };
