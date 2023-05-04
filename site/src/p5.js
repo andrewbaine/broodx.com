68b4498ecab0da561bb5c5bdd82e9a733aa6ae4c
Genesis Blockhead <genesisblockhead@protonmail.com>
2023-05-04 08:49:13 -0400

    close to working, just need to work out one more angle

--- site/src/p5.js
+++ site/src/p5.js
@@ -3,6 +3,7 @@ let p5 = require("p5");
 let sin = Math.sin;
 let cos = Math.cos;
 let tan = Math.tan;
+
 const root2 = Math.sqrt(2);
 
 const containerElement = document.getElementById("main");
@@ -16,6 +17,16 @@ let cartesian = (r, theta) => {
 
 const sketch = (p) => {
   let PI = p.PI;
+  const π = PI;
+  const translatePolar = (r, θ) => {
+    return {
+      x: r * cos(θ),
+      y: r * sin(θ),
+    };
+  };
+
+  const θ = p.PI / 12;
+  const c = (1 - tan(θ)) / (Math.sqrt(2) * sin((3 * PI) / 4 - 2 * θ));
   let size = 250;
   p.setup = function () {
     p.createCanvas(600, 600, p.WEBGL);
@@ -54,35 +65,40 @@ const sketch = (p) => {
   let stage3 = (n) => {
     p.background(200);
     p.scale(size / root2);
-    //    p.clear();
-    let theta = p.PI / 12;
-    let oneMinusTanTheta = 1 - tan(theta);
-    let a = oneMinusTanTheta;
-    let b =
-      (oneMinusTanTheta * sin(2 * theta)) / sin((3 * p.PI) / 4 - 2 * theta);
+
+    let oneMinusTanθ = 1 - tan(θ);
+    let a = oneMinusTanθ;
+    let b = (oneMinusTanθ * sin(2 * θ)) / sin((3 * p.PI) / 4 - 2 * θ);
+
+    p.fill("lightblue");
+    p.circle(0, 0, 0.05);
+    let translation = cartesian(oneMinusTanθ, (-3 * PI) / 4);
+    p.translate(translation.x, translation.y);
+    p.circle(0, 0, 0.05);
 
     p.fill("pink");
-    p.circle(250, 250, 40);
-
-    p.push();
     p.rotateZ(PI / 4);
+    let pA = cartesian(1 / cos(θ), -(PI / 2 + θ));
+    p.triangle(0, 0, oneMinusTanθ, 0, pA.x, pA.y);
+
+    let α = p.fill("lightblue");
+    p.rotateZ(-(PI / 2 + θ));
+    p.rotateX(-n * PI);
+    let point = cartesian(tan(θ), θ - PI / 2);
+    let point2 = cartesian(1 / cos(θ), 2 * θ - PI);
+    p.triangle(0, 0, 1 / cos(θ), 0, point2.x, point2.y);
+
+    p.fill("lightyellow");
+    p.rotateZ(2 * θ - π);
+    p.rotateX(-n * PI);
+    point = cartesian(c, θ - PI / 2);
+    p.triangle(0, 0, 1 / cos(θ), 0, point.x, point.y);
+
+    p.fill("lightgreen");
+    p.rotateZ(θ - PI / 2);
     p.rotateX(n * PI);
-    p.circle(250, 250, 40);
-    let pA = cartesian(a, PI);
-    let pB = cartesian(b, (3 * PI) / 4);
-    p.triangle(0, 0, pA.x, pA.y, pB.x, pB.y);
-    p.pop();
-
-    p.push();
-    p.translate(0, -root2);
-    p.rotateZ(PI / 4 - theta);
-
-    p.translate(0, 1 / cos(theta));
-
-    p.rotateY(n * PI);
-    let point = cartesian(2, theta + PI / 2);
-    p.triangle(0, 0, 0, 1 / cos(theta), point.x, point.y);
-    p.pop();
+    point = cartesian(oneMinusTanθ, -2 * θ);
+    p.triangle(0, 0, c, 0, point.x, point.y);
   };
 
   let stages = [
@@ -91,15 +107,15 @@ const sketch = (p) => {
       draw: () => {},
     },
     {
-      duration: 100,
+      duration: 0,
       draw: stage1,
     },
     {
-      duration: 1000,
+      duration: 0,
       draw: stage2,
     },
     {
-      duration: 2000,
+      duration: 3000,
       draw: stage3,
     },
   ];
